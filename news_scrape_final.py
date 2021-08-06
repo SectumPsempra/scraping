ffb405b31a4600d2dfd1b25c512154b6aaa52ce6
amit_97 <bhattamit991@gmail.com>
2021-08-06 09:31:29 +0530

    add user inputs, modfiy groups

---
+++ news_scrape_final.py
@@ -0,0 +1,177 @@
+import requests
+import bs4
+
+
+# Specify topics and optional keyword to check in url
+TOPICS = []
+
+# TOPIC_PREFIX = "news: "
+SEARCH_TAB = "nws"
+SEARCH_ENGINE_URL = "https://google.com"
+SEARCH_ENGINE_SOURCE_ID = ""
+
+# Specify groups based on news urls
+GROUPS = {
+    "washingtonpost.com": "USA", "edition.cnn.com": "USA",
+    "nytimes.com": "USA", "bbc.com": "USA", "scmp.com": "ASIA",
+    "globaltimes.cn": "ASIA", "chinadaily.com.cn": "ASIA",
+    "thediplomat.com": "ASIA"
+}
+OTHER_GROUP = "OTHERS"
+
+# Increase this number to get more results
+RESULT_PER_PAGE = 3
+
+# Final results, pass this dict as input to ml model 
+RESULT = {}
+
+# Invalid urls to filter out 
+FILTER_URLS = (
+    "maps.google",
+    "policies.google",
+    "accounts.google",
+    "/preferences?",
+    "support.google"
+)
+
+
+def _get_search_string(topic, tab="nws", topic_prefix="",
+        parent_url="https://google.com", location_source_id="",
+        result_per_page=RESULT_PER_PAGE):
+    """Returns search url.
+
+    Args:
+        topic (str): topic to search
+        parent_url (str): search engine and domain to use
+        location_source_id (str): unique identifier to specify location if
+            default parent_url is not used
+        result_per_page (int): number of results per page 
+    Returns:
+        url (str): url search string
+    """
+    url = f"{parent_url}/search?q={topic_prefix+topic}&{location_source_id}"\
+        f"&tbm={tab}&num={result_per_page}"
+
+    return url
+
+
+def _get_soup_object(url):
+    """Parse the given url.
+
+    Args:
+        url (str): takes url to parse and build the soup object
+    Returns:
+        soup (bs4.BeautifulSoup): bs4 object contiaing parsed html
+    """
+    request_result=requests.get(url)
+    soup = bs4.BeautifulSoup(request_result.text, "html.parser")
+    return soup
+
+
+def _get_urls(soup, keyword=None):
+    """Get news links from the google search.
+
+    Args:
+        soup (bs4.BeautifulSoup): soup object to iterate over
+        keyword (str): word to search inside url string, if keyword found
+            in the url, it is a valid url
+    Returns:
+        valid_urls (list): list of valid urls from the google result
+    """
+    valid_urls = []
+    tag = soup.find_all('a')
+    for text in tag:
+        href_text = text.get('href')
+        url = href_text[href_text.find('http'):]
+        if keyword and keyword not in url:
+            pass
+        else:
+            if "http" in url and not any(
+                invalid_url in url for invalid_url in FILTER_URLS
+            ):
+                valid_urls.append(url)
+    return valid_urls
+
+
+def _filter_duplicate_urls(urls):
+    unique_urls = []
+    clean_urls = set()
+    for url in urls:
+        cleaned_url = url.split("&sa=U")[0]
+        if cleaned_url not in clean_urls:
+            unique_urls.append(url)
+        clean_urls.add(cleaned_url)
+    return unique_urls
+
+
+def _get_content(soup, keyword=None):
+    """Get content from each news url if article is valid.
+
+    Args:
+        soup (bs4.BeautifulSoup): soup object to iterate over
+        keyword (str): word to search inside url string, if keyword found
+            in the url, it is a valid url
+
+    Returns:
+        articles_list (list): list containing content of different articles
+    """
+    articles = {}
+    urls = _filter_duplicate_urls(_get_urls(soup, keyword))
+    for url in urls:
+        try:
+            response = requests.get(url)
+            if response.status_code == 200:
+                # Comment/Uncomment below line to
+                # hide/print url of each article
+                url_soup = _get_soup_object(url)
+                article_tags = url_soup.find_all('article')
+                p_tags = article_tags[0].find_all('p')
+                final_article = ''
+                list_paragraphs = []
+                for p in range(0, len(p_tags)):
+                    paragraph = p_tags[p].get_text()
+                    list_paragraphs.append(paragraph)
+                final_article = " ".join(list_paragraphs)
+                if final_article:
+                    articles[url] = final_article
+        except Exception as err:
+            pass
+    return articles
+
+
+def _format_result(articles, topic_count, topic_prefix, group_prefix):
+    for url in articles:
+        key = ""
+        for valid_url in GROUPS:
+            if valid_url in url:
+                key = GROUPS[valid_url]
+        if not key:
+            key = OTHER_GROUP
+        RESULT[group_prefix + str(key) + "_" + topic_prefix +
+            str(topic_count+1)] = articles[url]
+
+
+if __name__ == "__main__":
+    # Driver function
+    num_topics = int(input("Please enter number of topics you want to"
+                            " search: "))
+    for i in range(num_topics):
+        TOPICS.append(str(input(f"Enter topic no. {i+1}: ")))
+
+    print(f"Selected Topics: {TOPICS}\n")
+    
+    RESULT_PER_PAGE = int(input("Enter number of results you want per"
+        " page for each topic (default is 3 results/page, 10 results/page is"
+        " recommended for better results): "))
+
+    print("\nProcessing inputs.....\n")
+
+    topic_prefix = "TOP_"
+    group_prefix = "GRP_"
+    for topic_count, topic in enumerate(TOPICS):
+        url = _get_search_string(topic, SEARCH_TAB, "", SEARCH_ENGINE_URL,
+            SEARCH_ENGINE_SOURCE_ID, RESULT_PER_PAGE)
+        soup_obj = _get_soup_object(url)
+        articles = _get_content(soup_obj)
+        _format_result(articles, topic_count, topic_prefix, group_prefix)
+    print(RESULT)
